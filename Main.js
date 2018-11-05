9b4e4d3fad79ce554d52a26881c157a33108a4b6
Scripter17 <jameschristopherwise@gmail.com>
2018-11-05 13:47:49 -0500

    Renamed variables and a few other things. main.js should be a bit easier to read now.

--- Main.js
+++ Main.js
@@ -1,11 +1,18 @@
 window.onload=function(){
 	initTable();
 }
+function setAttributesNS(elem, attrs){ // call element.setAttributeNS in bulk
+	var x;
+	for (x in attrs){
+		elem.setAttributeNS(null, x, attrs[x]);
+	}
+	return elem;
+}
 function initTable(){
 	var y, x, // The period and group of each element
 		tr, td, // Periods and groups, respectively
-		n, // Generic iteration variable
-		e, // The current element whose cell is being worked on
+		elemN, // The current element
+		elemCell, // The cell of the current element
 		table; // The periodic table itself
 	table=document.createElement("table");
 	table.innerHTML=""; // If, for some reason, initTable is called more than once, this should make it not break.
@@ -18,73 +25,72 @@ function initTable(){
 		}
 		table.appendChild(tr);
 	}
+	
 	for (x=1; x<=18; x++){table.childNodes[0].childNodes[x].innerHTML=x;} // Add the period labels at the top
 	for (y=1; y<=7; y++){table.childNodes[y].childNodes[0].innerHTML=y;} // Add the group labels on the left
-	document.getElementById("tableCont").appendChild(table);
-	for (n in elems){ // Set table cell symbol and add the onclick event
-		e=table.childNodes[elems[n]["ypos"]].childNodes[elems[n]["xpos"]];
-		e.innerHTML=elems[n].symbol;
-		e.title=elems[n].name;
-		e.setAttribute("onclick","viewElement("+n+")"); // If I do e.onclick=function(){viewElement(n);} makes it always do Ogganesson (118).
-		e.classList.add("element");
+	
+	document.getElementById("pTableCont").appendChild(table);
+	for (elemN in elems){ // Set table cell symbol and add the onclick event
+		elemCell=table.childNodes[elems[elemN]["ypos"]].childNodes[elems[elemN]["xpos"]];
+		elemCell.innerHTML=elems[elemN].symbol;
+		elemCell.title=elems[elemN].name;
+		elemCell.setAttribute("onclick","viewElement("+elemN+")"); // If I do elemCell.onclick=function(){viewElement(elemN);} makes it always do Ogganesson (118).
+		elemCell.classList.add("element");
 	}
 }
-function setAttributesNS(elem, attrs){ // call element.setAttributeNS in bulk
-	var x;
-	for (x in attrs){
-		elem.setAttributeNS(null, x, attrs[x]);
-	}
-	return elem;
-}
-function viewElement(e){ // Using this function costs you $11.99 (The price of a plate of spaghetti)
+function viewElement(elem){ // Using this function costs you $11.99 (The price of a plate of spaghetti)
 	var svg, // The atom diagram thing
 		w, h, // Width and height of the diagram
-		n, d, x, // Generic iteration variables
+		viewElem, // class="view" element number
+		shell, // The current shell while rendering the diagram
 		txt, txtAttrs, // The atomic symbol in the diagram and its attributes
 		ring, ringAttrs, // Electron shells
-		elec, elecAttrs, // Electrons
+		elecN, elec, elecAttrs, // Electrons (Nth in shell, <circle>, and Attributes)
 		angle, // The angle of each electron in a shell
-		ions, // The ions of the element
+		ions, ionN, // The ions of the element and the nth ion of the element
 		span, // Each number in the ion list is its own span element
-		elem; // I can't use for (x of elems) in IE, so I need to do for (x=0; x<elems.length; x++){elem=elems[x]}
-	e=elems[e]; // You may think this is stupid, but due to how I had to set the onclick functions, this is the only not-shit way to do it.
+		data; // I can't use for (x of elems) in IE, so I need to do for (x=0; x<elems.length; x++){elem=elems[x]}
+	
+	elem=elems[elem]; // You may think this is stupid, but due to how I had to set the onclick functions, this is the only not-shit way to do it.
+	
 	svg=document.getElementById("diagram");
-	while (svg.firstChild) {
+	while (svg.firstChild){ // Remove every ring/electron in the diagram
 		svg.removeChild(svg.firstChild);
 	}
+	
 	// Width and height of diagram (Right now, they're constant, but they might change later)
 	w=parseInt(svg.getAttribute("width"));
 	h=parseInt(svg.getAttribute("height"));
 	// Adds the chemical symbol to the diagram.
 	txtAttrs={"x":"50%", "y":"50%", "style":"text-anchor:middle; font-size:16px; transform:translate(0,0.25em)"};
 	txt=setAttributesNS(document.createElementNS("http://www.w3.org/2000/svg", "text"), txtAttrs);
-	txt.innerHTML=e.symbol;
+	txt.innerHTML=elem.symbol;
 	svg.appendChild(txt);
-	for (n=0; n<e.shells.length; n++){ // The shell loop.
-		ringAttrs={"cx":"50%", "cy":"50%", "r":(n+1)*5+"%", "style":"stroke: black; stroke-width:1px; fill:none;"};
+	for (shell=0; shell<elem.shells.length; shell++){ // The shell loop.
+		ringAttrs={"cx":"50%", "cy":"50%", "r":(shell+1)*5+"%", "style":"stroke: black; stroke-width:1px; fill:none;"};
 		ring=setAttributesNS(document.createElementNS("http://www.w3.org/2000/svg", "circle"), ringAttrs);
 		svg.appendChild(ring);
-		for (d=0; d<e.shells[n]; d++){ // The Electron loop
-			angle=d/e.shells[n]*2*Math.PI;
-			//elecAttrs={"cx":w/2+Math.sin(angle)*(n+1)*25, "cy":h/2-Math.cos(angle)*(n+1)*25, "r":"5", "style":"fill: black;"};
-			elecAttrs={"cx":(50+Math.sin(angle)*(n+1)*5)+"%", "cy":(50-Math.cos(angle)*(n+1)*5)+"%", "r":"1%", "style":"fill: black;"};
+		for (elecN=0; elecN<elem.shells[shell]; elecN++){ // The Electron loop
+			angle=elecN/elem.shells[shell]*2*Math.PI;
+			elecAttrs={"cx":(50+Math.sin(angle)*(shell+1)*5)+"%", "cy":(50-Math.cos(angle)*(shell+1)*5)+"%", "r":"1%", "style":"fill: black;"};
 			elec=setAttributesNS(document.createElementNS("http://www.w3.org/2000/svg", "circle"), elecAttrs);
 			svg.appendChild(elec);
 		}
 	}
 	// Set the data under the diagram
-	for (x=0; x<document.getElementsByClassName("view").length; x++){
-		elem=document.getElementsByClassName("view")[x];
-		elem.innerHTML=e[elem.id.split("_")[1]];
+	for (viewElem=0; viewElem<document.getElementsByClassName("view").length; viewElem++){
+		data=document.getElementsByClassName("view")[viewElem];
+		data.innerHTML=elem[data.id.split("_")[1]];
 	}
+	
 	// 100% jank method of listing ions with the common ones in blue
 	ions=document.getElementById("view_ions");
 	ions.innerHTML="";
-	for (x=0; x<e.ions[0].length; x++){
+	for (ionN=0; ionN<elem.ions[0].length; ionN++){
 		span=document.createElement("span");
-		span.innerHTML=e.ions[0][x];
-		// e.ions[0]=all ions, e.ions[1]=common ions
-		if (e.ions[1].indexOf(e.ions[0][x])!=-1){span.style.color="blue";}
+		span.innerHTML=elem.ions[0][ionN];
+		// elem.ions[0]=all ions, elem.ions[1]=common ions
+		if (elem.ions[1].indexOf(elem.ions[0][ionN])!=-1){span.style.color="blue";}
 		ions.appendChild(span);
 		ions.innerHTML+=",";
 	}
