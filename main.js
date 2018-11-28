30a0a9f4510b6737873a242986f8f9cd44e3b64d
Scripter17 <jameschristopherwise@gmail.com>
2018-11-28 15:03:30 -0500

    Reorganized and fixed stuff
    Also added the DBaD license.

--- main.js
+++ main.js
@@ -14,9 +14,8 @@ function initTable(){
 		elemN, // The current element
 		elemCell, // The cell of the current element
 		table; // The periodic table itself
-	table=document.createElement("table");
+	table=document.getElementById("periodicTable")
 	table.innerHTML=""; // If, for some reason, initTable is called more than once, this should make it not break.
-	table.id="periodicTable"; // Creative, I know.
 	for (y=0; y<11; y++){ // Periods
 		tr=document.createElement("tr");
 		for (x=0; x<19; x++){ // Groups
@@ -29,7 +28,6 @@ function initTable(){
 	for (x=1; x<=18; x++){table.childNodes[0].childNodes[x].innerHTML=x;} // Add the period labels at the top
 	for (y=1; y<=7; y++){table.childNodes[y].childNodes[0].innerHTML=y;} // Add the group labels on the left
 	
-	document.getElementById("pTableCont").appendChild(table);
 	for (elemN in elems){ // Set table cell symbol and add the onclick event
 		elemCell=table.childNodes[elems[elemN]["ypos"]].childNodes[elems[elemN]["xpos"]];
 		elemCell.innerHTML=elems[elemN].symbol;
@@ -50,7 +48,8 @@ function viewElement(elem){ // Using this function costs you $11.99 (The price o
 		ions, ionN, // The ions of the element and the nth ion of the element
 		span, // Each number in the ion list is its own span element
 		data; // I can't use for (x of elems) in IE, so I need to do for (x=0; x<elems.length; x++){elem=elems[x]}
-	
+	var rp=6; // Distance between electron shells as a percentage of diagram width
+	var ep=1.2; // Radius of electrons as a percentage of diagram width
 	elem=elems[elem]; // You may think this is stupid, but due to how I had to set the onclick functions, this is the only not-shit way to do it.
 	
 	svg=document.getElementById("diagram");
@@ -67,12 +66,12 @@ function viewElement(elem){ // Using this function costs you $11.99 (The price o
 	txt.innerHTML=elem.symbol;
 	svg.appendChild(txt);
 	for (shell=0; shell<elem.shells.length; shell++){ // The shell loop.
-		ringAttrs={"cx":"50%", "cy":"50%", "r":(shell+1)*5+"%", "style":"stroke: black; stroke-width:1px; fill:none;"};
+		ringAttrs={"cx":"50%", "cy":"50%", "r":(shell+1)*rp+"%", "style":"stroke: black; stroke-width:1px; fill:none;"};
 		ring=setAttributesNS(document.createElementNS("http://www.w3.org/2000/svg", "circle"), ringAttrs);
 		svg.appendChild(ring);
 		for (elecN=0; elecN<elem.shells[shell]; elecN++){ // The Electron loop
 			angle=elecN/elem.shells[shell]*2*Math.PI;
-			elecAttrs={"cx":(50+Math.sin(angle)*(shell+1)*5)+"%", "cy":(50-Math.cos(angle)*(shell+1)*5)+"%", "r":"1%", "style":"fill: black;"};
+			elecAttrs={"cx":(50+Math.sin(angle)*(shell+1)*rp)+"%", "cy":(50-Math.cos(angle)*(shell+1)*rp)+"%", "r":ep+"%", "style":"fill: black;"};
 			elec=setAttributesNS(document.createElementNS("http://www.w3.org/2000/svg", "circle"), elecAttrs);
 			svg.appendChild(elec);
 		}
